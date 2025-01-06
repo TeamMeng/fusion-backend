c14e751477e0af73ac0257d6d5bf09e4099b08c5
TeamMeng <862915265@qq.com>
2025-01-06 20:59:27 +0800

    chore: init project

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use anyhow::Result;
+use axum::{http::Method, response::IntoResponse, routing::post, Json, Router};
+use serde::{Deserialize, Serialize};
+use tokio::net::TcpListener;
+use tower_http::cors::{self, CorsLayer};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct User {
+    username: String,
+    email: String,
+    password: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let listener = TcpListener::bind("localhost:3000").await?;
+    println!("Server listening on localhost:3000");
+
+    let cors = CorsLayer::new()
+        .allow_methods([
+            Method::GET,
+            Method::POST,
+            Method::PATCH,
+            Method::DELETE,
+            Method::PUT,
+        ])
+        .allow_origin(cors::Any)
+        .allow_headers(cors::Any);
+
+    let app = Router::new()
+        .route("/register", post(index_handler))
+        .layer(cors);
+
+    axum::serve(listener, app).await?;
+
+    Ok(())
+}
+
+async fn index_handler(Json(input): Json<User>) -> impl IntoResponse {
+    println!("{:?}", input);
+    Json(input)
+}
